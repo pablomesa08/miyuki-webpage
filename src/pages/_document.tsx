79c1c67e41345ed09d1563491f3752c0f8a60374
Pablo Mesa <pablitomesa08@gmail.com>
2024-02-19 09:40:22 -0500

    Start

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Providers } from "@/app/providers";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <Providers>
+          <Main />
+          <NextScript />
+        </Providers>
       </body>
     </Html>
   );
