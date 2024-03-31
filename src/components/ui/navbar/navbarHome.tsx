fe4461748749cda2e89d05a2b6e156f02ee7f56f
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-03-31 09:16:49 -0500

    fix(Navbar): according to the wireframe

--- src/components/ui/navbar/navbarHome.tsx
+++ src/components/ui/navbar/navbarHome.tsx
@@ -8,6 +8,7 @@ import {
   NavbarItem,
   Link,
   Input,
+  Button,
 } from "@nextui-org/react";
 
 import AvatarProfile from "./avatarProfile/AvatarProfile.jsx";
@@ -18,9 +19,9 @@ import { mdiCartOutline, mdiMagnify } from "@mdi/js";
 import { AcmeLogo } from "./AcmeLogo.jsx";
 export default function NavbarHome() {
   const menuItemsRoutes = {
-    Tienda: "/store",
-    Conócenos: "/contact",
+    Productos: "/store",
     Promociones: "/promos",
+    Conócenos: "/contact",
   };
 
   return (
@@ -30,37 +31,28 @@ export default function NavbarHome() {
       </NavbarContent>
 
       <div className="flex-col w-full ">
-        <div className="flex justify-between w-full items-center">
-          <Input
-            classNames={{
-              base: "max-w-full sm:max-w-[12rem] h-10",
-              mainWrapper: "h-full",
-              input: "text-small",
-              inputWrapper:
-                "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-            }}
-            placeholder="Buscar productos..."
-            size="sm"
-            startContent={<Icon path={mdiMagnify} size={1} />}
-            type="search"
-          />
-          <div className="flex items-center mr-10 sm:mr-[11.5rem] ">
+        <div className="flex w-full items-center "></div>
+        <NavbarContent className="hidden mt-2 sm:flex gap-20" justify="center">
+          <div className="flex items-center ">
             <AcmeLogo />
             <p className="font-bold text-inherit">LOGO</p>
           </div>
-          <div className="flex items-center gap-2">
-            <AvatarProfile />
-            <Icon path={mdiCartOutline} size={1} />
+          <div className="flex flex-row items-center gap-3 ">
+            {Object.entries(menuItemsRoutes).map(([item, href], index) => (
+              <NavbarItem key={index} isActive={item === "Customers"}>
+                <Link color="foreground" href={href}>
+                  {item}
+                </Link>
+              </NavbarItem>
+            ))}
+            <div className="flex items-center gap-2">
+              <Button size="sm" variant="solid">
+                Iniciar sesión
+              </Button>
+              <Icon path={mdiMagnify} size={1} />
+              <Icon path={mdiCartOutline} size={1} />
+            </div>
           </div>
-        </div>
-        <NavbarContent className="hidden mt-2 sm:flex gap-40" justify="center">
-          {Object.entries(menuItemsRoutes).map(([item, href], index) => (
-            <NavbarItem key={index} isActive={item === "Customers"}>
-              <Link color="foreground" href={href}>
-                {item}
-              </Link>
-            </NavbarItem>
-          ))}
         </NavbarContent>
       </div>
 
