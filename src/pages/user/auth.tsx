d63157266143149382c19e10b324994011a94925
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-04-11 11:25:09 -0500

    beutify

--- src/pages/user/auth.tsx
+++ src/pages/user/auth.tsx
@@ -3,6 +3,7 @@ import { Button, Card, CardBody, Input, Image, Link } from "@nextui-org/react";
 export default function Auth() {
   return (
     <main className="flex flex-col min-h-[100vh] items-center justify-center">
+      <Link href="/">Regresar a página principal</Link>
       <section className="w-full flex flex-row justify-center grow items-center">
         <Card className="">
           <CardBody className="flex flex-row justify-evenly gap-10 flex-wrap items-center  w-[400px] sm:w-[600px] md:w-[800px] lg:w-[1000px]">
