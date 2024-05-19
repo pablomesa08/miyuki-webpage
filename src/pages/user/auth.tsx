1c142765ca7e08e27da293f49111dd7061dfd3ce
natynaro <90767304+natynaro@users.noreply.github.com>
2024-05-19 17:53:55 -0500

    auth y register

--- src/pages/user/auth.tsx
+++ src/pages/user/auth.tsx
@@ -1,6 +1,5 @@
 import { Card, CardBody, Input, Image, Link, Button } from "@nextui-org/react";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useState } from "react";
 import { FaEyeSlash } from "react-icons/fa";
 import { BsEyeFill } from "react-icons/bs";
 import { useAuth } from "@/hooks/useAuth";
@@ -32,7 +31,7 @@ export default function Auth() {
       setError("Por favor, ingrese un correo válido");
       return;
     }
-    login(credentials.email, credentials.password).catch((error) => {
+    login(credentials.email, credentials.password).catch(() => {
       setError("Error al iniciar sesión, intente de nuevo.");
     });
     setError("");
@@ -41,70 +40,70 @@ export default function Auth() {
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   return (
-    <main className="flex flex-col min-h-[100vh] items-center justify-center">
-      <Link href="/">Regresar a página principal</Link>
-      <section className="w-full flex flex-row justify-center grow items-center">
-        <Card>
-          <CardBody className="flex flex-row justify-evenly gap-10 flex-wrap items-center w-[400px] sm:w-[600px] md:w-[800px] lg:w-[1000px]">
-            <form onSubmit={handleLogin} className="flex flex-col gap-2">
-              <h1 className="font-bold text-center">¡Bienvenido!</h1>
-              <p>Usuario/correo:</p>
-              <Input
-                isRequired
-                placeholder="Ingrese su correo"
-                variant="bordered"
-                radius="full"
-                type="email"
-                label="Email"
-                name="email"
-                color={isEmailInvalid ? "danger" : "default"}
-                errorMessage={
-                  isEmailInvalid && "Por favor, ingrese un correo válido"
-                }
-                onChange={handleInputChange}
-              />
-              <p>Contraseña:</p>
-              <Input
-                placeholder="Ingrese su contraseña"
-                isRequired
-                endContent={
-                  <button
-                    className="focus:outline-none"
-                    type="button"
-                    onClick={toggleVisibility}
-                  >
-                    {isVisible ? (
-                      <FaEyeSlash className="text-2xl text-default-400 pointer-events-none" />
-                    ) : (
-                      <BsEyeFill className="text-2xl text-default-400 pointer-events-none" />
-                    )}
-                  </button>
-                }
-                type={isVisible ? "text" : "password"}
-                name="password"
-                variant="bordered"
-                radius="full"
-                onChange={handleInputChange}
-              />
-              <Button type="submit">Iniciar sesión</Button>
-              {error && (
-                <p className="text-danger mt-2 text-center w-64">{error}</p>
-              )}
-              <p>
-                ¿No tienes una cuenta?{" "}
-                <Link href="/user/register">Registrate</Link>
-              </p>
-            </form>
-            <section className="mt-5 mb-5">
-              <Image
-                alt="Random image"
-                src="https://source.unsplash.com/random/500x400"
-                width={500}
-                height={400}
-              />
-            </section>
-          </CardBody>
-        </Card>
+    <main className="flex flex-col min-h-screen w-full items-center justify-center bg-background no-shadow">
+      <Link href="/" className="absolute top-4 left-4 text-focus">Regresar a página principal</Link>
+      <section className="flex flex-col md:flex-row items-center justify-center gap-10 p-8 rounded-lg no-shadow w-full max-w-6xl">
+        <form onSubmit={handleLogin} className="flex flex-col gap-4 w-full max-w-md">
+          <h1 className="font-bold text-4xl text-center mb-8">¡Bienvenido!</h1>
+          <div className="flex flex-col gap-2 mt-8">
+            <label htmlFor="email" className="font-semibold">Usuario/correo:</label>
+            <Input
+              id="email"
+              isRequired
+              placeholder="Ingrese su correo"
+              variant="bordered"
+              radius="full"
+              type="email"
+              name="email"
+              color={isEmailInvalid ? "danger" : "default"}
+              errorMessage={
+                isEmailInvalid && "Por favor, ingrese un correo válido"
+              }
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password" className="font-semibold">Contraseña:</label>
+            <Input
+              id="password"
+              placeholder="Ingrese su contraseña"
+              isRequired
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={toggleVisibility}
+                >
+                  {isVisible ? (
+                    <FaEyeSlash className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <BsEyeFill className="text-2xl text-default-400 pointer-events-none" />
+                  )}
+                </button>
+              }
+              type={isVisible ? "text" : "password"}
+              name="password"
+              variant="bordered"
+              radius="full"
+              onChange={handleInputChange}
+            />
+          </div>
+          <Button type="submit" className="mt-10 bg-focus text-lg hover:bg-purple-700 text-white rounded-full">Iniciar sesión</Button>
+          {error && (
+            <p className="text-danger mt-2 text-center">{error}</p>
+          )}
+          <p className="mt-4 text-center">
+            ¿No tienes una cuenta?{" "}
+            <Link href="/user/register" className="text-green-600">Regístrate</Link>
+          </p>
+        </form>
+        <div className="flex justify-center items-center w-full md:w-1/2 hidden md:flex">
+          <Image
+            alt="Random image"
+            src="/Images/Backround/verde.png"
+            className="rounded-lg w-full h-auto"
+          />
+        </div>
       </section>
     </main>
   );
