7d8c696108b05e39d0ba9eb98d67e5d15ae34f3e
natynaro <90767304+natynaro@users.noreply.github.com>
2024-05-27 15:43:10 -0500

    links

--- src/components/ui/groupImages/threeImageGallery.tsx
+++ src/components/ui/groupImages/threeImageGallery.tsx
@@ -1,4 +1,5 @@
 import { Image } from "@nextui-org/react";
+import { useRouter } from "next/router";
 
 type ImageInfo = {
   imageUrl: string;
@@ -13,10 +14,23 @@ type ImageInfoProps = {
 };
 
 export default function ThreeImageGallery({ imageInfoArray, textColor }: Readonly<ImageInfoProps>) {
+  const router = useRouter();
+
+  const handleImageClick = (imageUrl: string) => {
+    const productId = imageUrl.split("/").pop()?.replace(".png", "");
+    if (productId) {
+      router.push(`/products/product?productId=${productId}`);
+    }
+  };
+
   return (
     <div className="flex flex-wrap justify-center items-center mt-5 mx-auto px-4 gap-8 md:gap-16 lg:gap-32 w-full" style={{ minHeight: '4rem' }}>
       {imageInfoArray.map((imageInfo, index) => (
-        <div key={index} className="flex flex-col items-center p-5 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+        <div 
+          key={index} 
+          className="flex flex-col items-center p-5 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 cursor-pointer"
+          onClick={() => handleImageClick(imageInfo.imageUrl)}
+        >
           <div className="relative w-full flex justify-center items-center overflow-hidden gap-8 md:gap-16">
             <div className="absolute inset-0 w-150 h-150 flex justify-center items-center overflow-hidden rounded-full">
               <Image
